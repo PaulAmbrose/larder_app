2fdac780c96745caf59d5c69ad37a125793c513e
Paul Ambrose <pablo_rose34@hotmail.com>
2022-10-25 20:10:24 +0100

    Update FoodClass.py

--- initial_prototyping/FoodClass.py
+++ initial_prototyping/FoodClass.py
@@ -1,1 +1,22 @@
+# Initial prototype for food class
+# Paul Ambrose - 25/10/2022
 
+
+class food():
+
+    def __init__(self, FoodType, FoodStoreLoc, FoodCost, FoodAmount, FoodMacroNut):
+        self.FoodType
+        self.FoodStoreLoc
+        self.FoodCost  # per 100g
+        self.FoodAmount  # in grams
+        self.FoodMacroNut  # CarbsProtenFat tuple for % of each e.g (10,10,80)
+
+        self.FoodType = FoodType,
+        self.FoodStoreLoc = FoodStoreLoc,
+        self.FoodCost = FoodCost,
+        self.FoodAmount = FoodAmount,
+        self.FoodMacroNut = FoodMacroNut
+
+    def FoodMoneyValue(self):
+        FoodMoneyValue = (self.FoodAmount/100) * self.FoodCost
+        return FoodMoneyValue
